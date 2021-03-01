da5cb6e183fc36711745a74e9706b8571a9577f0
DaShaun <dashaun@dashaun.com>
2021-03-01 12:44:57 -0600

    block with 3 module

--- azurerm_redis_enterprise_database.tf
+++ azurerm_redis_enterprise_database.tf
@@ -3,7 +3,9 @@ resource "azurerm_redis_enterprise_database" "redisgeek" {
   resource_group_name = azurerm_resource_group.redisgeek.name
   cluster_id = azurerm_redis_enterprise_cluster.redisgeek.id
   clustering_policy = var.azure_redis_enterprise_database_clustering_policy
-  module {
+  module = {
     name = "RediSearch"
+    name = "RedisTimeSeries"
+    name = "RedisBloom"
   }
 }
