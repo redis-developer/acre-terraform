f888deed69a2152f546d6a88f9891cb2f2df0b21
DaShaun <dashaun@redislabs.com>
2021-07-14 20:14:32 -0500

    update tags

--- azurerm_resource_group.tf
+++ azurerm_resource_group.tf
@@ -1,6 +1,5 @@
 resource "azurerm_resource_group" "redisgeek" {
   name     = format("redisgeek-%s", random_string.resource_group_name.result)
   location = var.location
-
-  tags = merge(var.tags, { name = "redisgeek" })
+  tags     = merge(var.tags, { owner = data.azurerm_client_config.current.client_id })
 }
